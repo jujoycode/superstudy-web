846ad252f580e49bc55bf40a7b083665b2757866
jh.ryu <jh.b.ryu@gmail.com>
2025-05-12 22:51:22 +0900

    Rendering Error

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,25 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-// import { QueryClientProvider } from "react-query";
-import { RecoilRoot } from "recoil";
-// import { GroupContainer } from "./container/group";
-// import { UserContainer } from "./container/user";
-// import { queryClient } from "./lib/query";
-import { App } from "./App";
-// import "./util/i18n";
-import "./calendar.css";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { QueryClientProvider } from 'react-query'
+import { RecoilRoot } from 'recoil'
+import { GroupContainer } from '@/legacy/container/group'
+import { UserContainer } from '@/legacy/container/user'
+import { queryClient } from '@/legacy/lib/query'
+import { App } from './App'
+import './legacy/util/i18n'
+import './legacy/calendar.css'
+import './legacy/index.css'
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RecoilRoot>
-    {/* <QueryClientProvider client={queryClient}> */}
-        {/* <UserContainer.ContextProvider> */}
-          {/* <GroupContainer.ContextProvider> */}
+      <QueryClientProvider client={queryClient}>
+        <UserContainer.ContextProvider>
+          <GroupContainer.ContextProvider>
             <App />
-          {/* </GroupContainer.ContextProvider> */}
-        {/* </UserContainer.ContextProvider> */}
-      {/* </QueryClientProvider> */}
+          </GroupContainer.ContextProvider>
+        </UserContainer.ContextProvider>
+      </QueryClientProvider>
     </RecoilRoot>
-  </StrictMode>
-);
+  </StrictMode>,
+)
