64a9e2a115ae4dae5d5c6da369082bb8f051a816
jujoycode <jh.b.ryu@gmail.com>
2025-05-14 11:09:17 +0900

    edit: notification-warning

--- src/stores/notification.ts
+++ src/stores/notification.ts
@@ -2,10 +2,12 @@ import { create } from 'zustand'
 
 interface NotificationState {
   toast: string | undefined
+  warning: string | undefined
   newsletterOpenedGroup: string[]
   newMsgCnt: number
 
   setToast: (toast: string) => void
+  setWarning: (toast: string) => void
   setNewsletterOpenedGroup: (groups: string[]) => void
   setNewMsgCnt: (count: number) => void
   reset: () => void
@@ -14,6 +16,7 @@ interface NotificationState {
 // 초기 상태
 const initialState = {
   toast: undefined,
+  warning: undefined,
   newsletterOpenedGroup: [],
   newMsgCnt: 0
 }
@@ -23,6 +26,7 @@ export const useNotificationStore = create<NotificationState>()((set) => ({
 
   // 액션
   setToast: (toast) => set({ toast }),
+  setWarning: (warning) => set({ warning }),
   setNewsletterOpenedGroup: (newsletterOpenedGroup) => set({ newsletterOpenedGroup }),
   setNewMsgCnt: (newMsgCnt) => set({ newMsgCnt }),
   reset: () => set(initialState),
