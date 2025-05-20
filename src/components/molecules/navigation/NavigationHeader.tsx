7f9a39513ee5bc5f5247866d735aeaf3c2f17421
jujoycode <jh.b.ryu@gmail.com>
2025-05-21 01:17:36 +0900

    Edit: LNB

--- src/components/molecules/navigation/NavigationHeader.tsx
+++ src/components/molecules/navigation/NavigationHeader.tsx
@@ -1,4 +1,5 @@
 import { Container } from '@/atoms/Container'
+import { Divider } from '@/atoms/Divider'
 import { Icon } from '@/atoms/Icon'
 import { NavigationProfile, type NavigationProfileProps } from '@/molecules/navigation/NavigationProfile'
 
@@ -8,19 +9,23 @@ export type NavigationHeaderProps = {
 
 export function NavigationHeader({ ProfileProps }: NavigationHeaderProps) {
   return (
-    <Container flex gap="4">
-      {/* Logo & Language Icon & Notification Icon */}
-      <Container flex direction="row" items="center" justify="end" width="184px" noPadding>
-        <Icon name="logo" customSize={{ width: '70px', height: '40px' }} />
+    <Container noPadding fixed>
+      <Container flex paddingX="5" gap="4" className="mt-8">
+        {/* Logo & Language Icon & Notification Icon */}
+        <Container flex direction="row" items="center" justify="end" width="15%" noPadding>
+          <Icon name="logo" customSize={{ width: '70px', height: '40px' }} />
 
-        <Container flex direction="row" justify="end" items="end" gap="3" noPadding>
-          <Icon name="world" customSize={{ width: '20px', height: '20px' }} />
-          <Icon name="bell" customSize={{ width: '20px', height: '20px' }} />
+          <Container flex direction="row" justify="end" items="end" gap="3" noPadding>
+            <Icon name="world" customSize={{ width: '20px', height: '20px' }} />
+            <Icon name="bell" customSize={{ width: '20px', height: '20px' }} />
+          </Container>
         </Container>
+
+        {/* Avatar & Name & Email & School Name */}
+        <NavigationProfile {...ProfileProps} />
       </Container>
 
-      {/* Avatar & Name & Email & School Name */}
-      <NavigationProfile {...ProfileProps} />
+      <Divider marginY="4" />
     </Container>
   )
 }
