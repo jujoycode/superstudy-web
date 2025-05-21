d7c3583261d77926b1cc01bbfda3476be5f1c768
Suh Jihun <jihunsuh12@gmail.com>
2025-05-21 22:07:50 +0900

    일정 cursor-pointer 추가

--- src/legacy/components/calendar/LnbCalendarsItem.tsx
+++ src/legacy/components/calendar/LnbCalendarsItem.tsx
@@ -9,7 +9,7 @@ interface LnbCalendarsItemProps {
 export function LnbCalendarsItem({ value, checked, color, text, onClick }: LnbCalendarsItemProps) {
   return (
     <div onClick={onClick}>
-      <label>
+      <label className="cursor-pointer">
         <input type="checkbox" className="tui-full-calendar-checkbox-round" value={value} checked={checked} />
         <span style={{ borderColor: color, backgroundColor: color }}></span>
         <span style={{ lineHeight: '1.5rem' }}>{text}</span>
