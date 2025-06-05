fcaf64381bc6fcd06cd0ebc99269e0db905f98c0
jh.ryu <jh.b.ryu@gmail.com>
2025-06-05 12:49:40 +0900

    Edit: Logo (m+g)

--- src/components/molecules/navigation/NavigationHeader.tsx
+++ src/components/molecules/navigation/NavigationHeader.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router'
+import LogoPink from '@/assets/images/logo_color_pink.png'
 import { Flex } from '@/atoms/Flex'
 import { Icon } from '@/atoms/Icon'
 import { NavigationLanguageDropdown } from '@/molecules/navigation/NavigationLanguageDropdown'
@@ -18,12 +19,13 @@ export function NavigationHeader({ ProfileProps }: NavigationHeaderProps) {
   return (
     <Flex justify="center" items="center" direction="col">
       <Flex direction="row" justify="between" items="center" gap="3" className="p-5 pt-8">
-        <Icon
+        {/* <Icon
           name="logo"
           customSize={{ width: '70px', height: '40px' }}
           onClick={handleLogoClick}
           className="cursor-pointer"
-        />
+        /> */}
+        <img src={LogoPink} alt="logo" className="w-24 contain-content" onClick={handleLogoClick} />
 
         <Flex direction="row" justify="end" items="center" gap="3">
           <NavigationLanguageDropdown />
